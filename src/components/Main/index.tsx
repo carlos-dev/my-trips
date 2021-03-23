a047fc186e19a99d330e43150c0c1278e151446f
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-03-23 08:58:14 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src='/img/logo.svg' />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>TypeScript, React, NextJS e styled</S.Description>
+    <S.Illustration src='/img/hero-illustration.svg' />
+  </S.Wrapper>
+)
+
+export default Main
