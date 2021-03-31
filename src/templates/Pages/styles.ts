c105ead6f0012801d4ede49ccfa8f341ead45520
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-03-31 09:25:06 -0300

    Utilizando MapConsumer do Leaflet para controlar mapa programaticamente

--- src/templates/Pages/styles.ts
+++ src/templates/Pages/styles.ts
@@ -6,7 +6,6 @@ export const Content = styled.section`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  height: 100vh;
   max-width: var(--container);
   margin: auto;
 `
